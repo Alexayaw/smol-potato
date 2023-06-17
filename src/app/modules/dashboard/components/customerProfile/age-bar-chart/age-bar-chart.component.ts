165949537d14b187f6b9f522a31cafb2e8b63c89
Boey01 <boeyshuwei@gmail.com>
2023-06-17 23:55:04 +0800

    backup for grid testing

--- src/app/modules/dashboard/components/customerProfile/age-bar-chart/age-bar-chart.component.ts
+++ src/app/modules/dashboard/components/customerProfile/age-bar-chart/age-bar-chart.component.ts
@@ -4,44 +4,40 @@ import { ChartOptions } from '../../../../../shared/models/chart-options';
 @Component({
   selector: 'app-age-bar-chart',
   templateUrl: './age-bar-chart.component.html',
-  styleUrls: ['./age-bar-chart.component.scss']
+  styleUrls: ['./age-bar-chart.component.scss'],
 })
 export class AgeBarChartComponent {
   public chartOptions: Partial<ChartOptions>;
 
-  
-    constructor() {
-      const ageData = [
-        { age: "18-24", count: 15 },
-        { age: "25-34", count: 32 },
-        { age: "35-44", count: 28 },
-        { age: "45-54", count: 20 },
-        { age: "55+", count: 12 }
-      ]
-      // Extracting data for chart
-      const categories = ageData.map(data => data.age);
-      const counts = ageData.map(data => data.count);
+  constructor() {
+    const ageData = [
+      { age: '18-24', count: 15 },
+      { age: '25-34', count: 32 },
+      { age: '35-44', count: 28 },
+      { age: '45-54', count: 20 },
+      { age: '55+', count: 12 },
+    ];
+    // Extracting data for chart
+    const categories = ageData.map((data) => data.age);
+    const counts = ageData.map((data) => data.count);
 
-      this.chartOptions = {
-              chart: {
-        type: "bar",
+    this.chartOptions = {
+      chart: {
+        type: 'bar',
+        width: '100%',
       },
       series: [
         {
-          name: "Age Count",
-          data: counts
-        }
+          name: 'Age Count',
+          data: counts,
+        },
       ],
       xaxis: {
         categories: categories,
       },
       legend: {
-        show: true
-      }
-      }
-
-    }
-
+        show: true,
+      },
+    };
   }
-
-
+}
