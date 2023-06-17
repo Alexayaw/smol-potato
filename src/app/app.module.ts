1c1c64f48221f21671386fd03a142b96c9f079a7
YAW XIN YING <TP054359@mail.apu.edu.my>
2023-06-17 14:54:23 +0800

    resolve component not found issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
-  providers: [
-
-  ],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
