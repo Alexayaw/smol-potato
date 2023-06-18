655f592850d908944324294776846b3cc6073d2d
Li Weiiii <eweliwei1d5@gmail.com>
2023-06-18 08:12:05 +0800

    Word Cloud (Alternative)

--- src/app/modules/dashboard/components/co/co-wordcloud/co-wordcloud.component.ts
+++ src/app/modules/dashboard/components/co/co-wordcloud/co-wordcloud.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as d3 from 'd3';
-import cloud from 'd3-cloud'; 
+// import cloud from 'd3-cloud'; 
 @Component({
   selector: '[co-wordcloud-chart-card]',
   templateUrl: './co-wordcloud.component.html',
@@ -43,15 +43,15 @@ export class CoWordcloudComponent implements OnInit {
       .domain([0, maxCount])
       .range([10, 50]); // Adjust the range to control the font size
 
-    const layout = cloud.cloud()
-      .size([width, height])
-      .words(data.map((d: any) => ({ text: d.word, size: fontSizeScale(d.count) })))
-      .padding(5)
-      .rotate(() => Math.random() < 0.5 ? 0 : 90)
-      .fontSize((d: any) => d.size)
-      .on('end', draw);
+    // const layout = cloud.cloud()
+    //   .size([width, height])
+    //   .words(data.map((d: any) => ({ text: d.word, size: fontSizeScale(d.count) })))
+    //   .padding(5)
+    //   .rotate(() => Math.random() < 0.5 ? 0 : 90)
+    //   .fontSize((d: any) => d.size)
+    //   .on('end', draw);
 
-    layout.start();
+    // layout.start();
 
     function draw(words: any[]) {
       svg.append('g')
